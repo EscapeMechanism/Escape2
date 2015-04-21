69831f660ed2f1ac4fba36356257da594d8f9202
Simon Hildebrandt <simonhildebrandt@gmail.com>
2015-04-21 11:06:52 +1000

    Added texture

--- Assets/Scripts/WheelerController.cs
+++ Assets/Scripts/WheelerController.cs
@@ -7,27 +7,26 @@ public class WheelerController : MonoBehaviour {
 	public float torque;
 	public float speed_limit = 500f;
 	GravityHandler gravity;
+	Rigidbody2D body;
 
 	// Use this for initialization
 	void Start () {
-		gravity = GetComponent<GravityHandler> () as GravityHandler;
+		gravity = GetComponent<GravityHandler> ();
+		body = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		drive = Input.GetAxis("Horizontal");
-		torque = GetComponent<Rigidbody2D>().angularVelocity;
+		torque = body.angularVelocity;
 //		if ((torque > -speed_limit && drive > 0f) || (torque < speed_limit && drive < 0f)) {
 //			rigidbody2D.AddTorque(drive * -500f);
 //		}
-		GetComponent<Rigidbody2D>().AddTorque(drive * acceleration());
-		if (drive == 0f) {
-			GetComponent<Rigidbody2D>().AddTorque(torque/-20f);
-		}
+		body.AddTorque(drive * acceleration());
 	}
 
 	float acceleration() {
-		return 1000f * direction();
+		return 10f * direction();
 	}
 
 	int direction() {
